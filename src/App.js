57a1f3b2535a0c2469ec3809cc58ae2fbf09ff02
Dani Alamo <dca5244@psu.edu>
2023-02-20 23:58:35 -0500

    React Card Repo
    Ported HTML and CSS code onto the react repo

--- src/App.js
+++ src/App.js
@@ -3,22 +3,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div> 
+
+<div class="buttons">
+  <button class="duplicate">Duplicate card</button>
+  <button class="changeTitle">Change Name</button>
+  <button id="deletelastcard">Delete</button>
+  <button data-toggle-btn>Toggle stats</button>
+</div>
+
+<div class= "wrapper"> 
+  <h2>Week 2 Homework</h2> 
+     <img src="https://cms.nhl.bamgrid.com/images/photos/334580860/1024x576/cut.jpg"/> 
+
+  <div id= "textbox1">
+    <p>The actual best team in the NHL.</p>
+  </div>
+
+  <details>
+    <summary>More Facts</summary>
+    Haters gonna hate.
+ </details>
+
+  <div class= "btn">
+    <a href = "https://hax.psu.edu/">
+  <button>Details</button> 
+    </a>
+  </div> 
+  <div id= "textbox2">
+    <p> Cards by Dani</p>
+  </div>
+
+</div>
+   
+</div>    
+   
   );
 }
 
